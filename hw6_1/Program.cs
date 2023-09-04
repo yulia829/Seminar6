620aa846cb2332bc2ceb23913c6bfa417ce5b188
Yulia <meshokyulia0002@gmail.com>
2023-09-04 20:04:42 +0300

    hw61

---
+++ hw6_1/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Введите количество элементов массива: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[M];
+FillArray(M);
+
+void FillArray(int M)
+{
+for (int i = 0; i < M; i++)
+{
+Console.WriteLine($"Введите {i+1} элемент массива ");
+array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+}
+
+int count(int[] array)
+{
+int i=0;
+int sum = 0;
+while (i < array.Length)
+{
+if(array[i]>0)
+sum = sum + 1;
+i = i + 1;
+}
+return sum;
+}
+Console.Write($"Чисел больше нуля: {count(array)}");
