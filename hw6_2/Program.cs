cea81f5ca467a92ecb5c72eec49c26fab85abdfa
Yulia <meshokyulia0002@gmail.com>
2023-09-04 20:07:52 +0300

    hw62

---
+++ hw6_2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите число b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x =(-b2 + b1)/(-k1 + k2) ;  // -(b1 — b2) / (k1 — k2);
+double y = k1 * x + b1;
+Console.WriteLine($"Точка пересечения двух прямых: [{x};{y}]");
